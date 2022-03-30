2475c02cde1d4da5f57cda59480b21cb5f46c61b
nguyentommycs <nguyentommycs@gmail.com>
2022-03-30 00:45:29 -0700

    completed seat algorithm and file outputs

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # theater-seating
 
-<a name="project-setup"></a>
-# Project Setup
+# Setup
 
 1. Clone this repo 
 ```sh
 git clone https://github.com/nguyentommycs/theater-seating.git
 ``` 
-2. create a virtual environment
+2. Create a virtual environment
 ```sh
 python -m venv env
 ``` 
@@ -19,12 +18,12 @@ env\Scripts\activate.bat
 ```sh
 source env/bin/activate
 ```
-4. Run the program by passing in the input file path. Use this command to run the sample input, or replace ```.\sampleinput ``` with the full path to your input.
+4. Run the program by passing in the input file path. Use this command to run the sample input, or replace ```.\sampleinput``` with the full path to your input.
 ```sh
 python app.py .\sampleinput
 ```
 # Assumptions
 1. Middle rows are preferred to outer rows (i.e. rows E>F>D>G>C>H...). Earlier requests will receieve seats as close to middle row as possible.
 2. While middle column seats (i.e. seats 10,11) may be slightly preferred to edge seats, the algorithm will assign seats left to right to better accomodate big groups.
-3. Reservation groups will not be split up unless there are no remaining ways to fit them. If they are split, they will be split into as few groups as possible. For simplicity, we will value fewer split groups over more evenly split groups (i.e. a group of 9 would be split 8-1 rather than 3-3-3).
+3. Reservation groups will not be split up unless there are no remaining ways to fit them. If they are split, they will be split into as few groups as possible. For simplicity, we will simply break up the group into any available seats, going from the middle rows outwards. (This is probably not the best way, but is being done for the sake of time.)
 4. If a reservation group is larger than the total amount of remaining seats, they will not be seated. (i.e. 18 people out of a group of 20 will not be seated if there are only 18 seats left)
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import sys
-
-
+import pathlib
+from datetime import datetime
 def file_to_arr(file_path):
     """
     Returns: array containing each seating request 
@@ -27,22 +27,33 @@ def result_to_file(result):
     Returns: file path
     
     This function writes the result string to a new text file 
-    and returns the path to the file
+    and returns the path to the file. The file will include the 
+    date and time the results were generated
     
     Parameter result: string containing seating assignments
     Precondition: valid string
 
+
     """
-    pass
+    #label file with date and time
+    now = datetime.now()
+    dt_str = now.strftime("%m-%d-%Y %H-%M-%S")
+    file = open("results "+dt_str+".txt","w")
+    file.write(result)
+    return str(pathlib.Path().resolve()) + "\\results " +dt_str+".txt"
+    
+    
 
 
 
 
 # grab file path from arguments
 file_path = str(sys.argv[1])
+
 #convert to ints representing requests
 requests = file_to_arr(file_path)
-num_to_row = {0:"A",1:"B",2:"C",3:"D",4:"E",5:"F",6:"G",7:"H",8:"I",9:"J"}
+
+NUM_TO_ROW = {0:"A",1:"B",2:"C",3:"D",4:"E",5:"F",6:"G",7:"H",8:"I",9:"J"}
 ROW_CAPACITY = 20
 NUM_ROWS = 10
 BUFFER = 3
@@ -63,31 +74,58 @@ for i in range(len(requests)):
     request = requests[i]
 
     if total_cap>=request:
+        completed_request = False
+
+        #generate the string for the result
+        temp_str = "\nR"
+        if i<99:
+            temp_str = temp_str+"0"
+        if i<9:
+            temp_str = temp_str+"0"
+        temp_str = temp_str + str(i+1) + " "
+
         for row in row_pref:
             row_cap = row_caps[row]
 
             if row_cap>=request:
                 #we can fit the entire request in one row
-                #generate the string for the result
-                temp_str = "\nR"
-                if i<99:
-                    temp_str = temp_str+"0"
-                if i<9:
-                    temp_str = temp_str+"0"
-                temp_str = temp_str + str(i+1) + " "
+                #append seats to the result string
                 for j in range(request):
                     if j == 0:
-                        temp_str = temp_str + str(num_to_row[row]) + str(j+ROW_CAPACITY-row_cap+1)
+                        temp_str = temp_str + str(NUM_TO_ROW[row]) + str(j+ROW_CAPACITY-row_cap+1)
                     else:
-                        temp_str = temp_str +","+ str(num_to_row[row]) + str(j+ROW_CAPACITY-row_cap+1)
-                seating_str=seating_str+temp_str
+                        temp_str = temp_str +","+ str(NUM_TO_ROW[row]) + str(j+ROW_CAPACITY-row_cap+1)
+                
 
                 #update our total and row by row capacity
                 total_cap = total_cap - min(request+BUFFER,row_cap)
                 row_caps[row]=row_cap-(request+BUFFER)
+                completed_request=True
                 break
         
         #if we get here, the group can't fit as one whole unit, but they
         #can fit if they get broken up
-        
+        if not completed_request:
+            for row in row_pref:
+                row_cap = row_caps[row]
+                if row_cap>0:
+
+                    for j in range(min(request,row_cap)):
+                        temp_str = temp_str +","+ str(num_to_row[row]) + str(j+ROW_CAPACITY-row_cap+1)
+                    #update our total and row by row capacity
+                    total_cap = total_cap - min(request+BUFFER,row_cap)
+                    row_caps[row]=row_cap-(request+BUFFER)
+                    request = request - row_cap
+                if request <=0:
+                    break
+            #remove an extra comma
+            temp_str = temp_str[0:6]+temp_str[7:]
+        seating_str=seating_str+temp_str
+            
+
+
+#remove extra newline at beginning of str
+if seating_str!="":
+    seating_str =seating_str[1:]
 print(seating_str)
+print(result_to_file(seating_str))
